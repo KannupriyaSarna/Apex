b1d0c143ff37e335e3110498b7486f945d42684c
KannupriyaSarna <45926531+KannupriyaSarna@users.noreply.github.com>
2019-05-12 12:47:57 -0700

    New Start at Apex.
    Signed-off-by: KannupriyaSarna
    <45926531+KannupriyaSarna@users.noreply.github.com>

---
+++ NewClassApex/src/MyName.java
@@ -0,0 +1,32 @@
+
+public class MyName {
+	
+	public static void main (String[] args) {
+		
+		String name = "Kannu";
+		int age = 30;
+		String ae1 = "30";
+		boolean ae2 = true;
+		Double ae3 = 41.41;
+		byte ae4 = 127;
+		short ae5 = 10000;
+		long ae6 = 1000000000;
+		float ae7 = 12.21f;
+		char ae8 = 'A';
+		int ae9 = 10+20;
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(ae1);
+		System.out.println(ae2);
+		System.out.println(ae3);
+		System.out.println(ae4);
+		System.out.println(ae5 );
+		System.out.println(ae6);
+		System.err.println(ae7);
+		System.out.println(ae8);
+		System.out.println(name+ " test "+ ae1 +" "+ae2 +" "+ ae3+" "+ ae4 +" "+ae5+ " "+ ae6+ " "+ae7 +" ");
+		System.out.println(ae9);
+	}
+
+}
